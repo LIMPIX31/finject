4d48d69c6427e3fe2797c18151702d38973ee1d9
LIMPIX31 <limpix31@gmail.com>
2022-06-08 13:06:49 +0200

    Prototype support

--- index.ts
+++ index.ts
@@ -6,14 +6,34 @@ export interface Metadata<T> {
   noreturn: () => void
 }
 
-export default <T extends (...args: any[]) => any, P extends Parameters<T>>(target: T, cb: (meta: Metadata<T>, ...args: P) => ReturnType<T> | undefined | void): T =>
-  ((...args: P) => {
-    let noreturn
-    const metadata: Metadata<T> = {
-      stacktrace: parse(new Error()).slice(1),
-      target,
-      noreturn: () => noreturn = true
+export interface PrototypeMetadata<T, O> extends Metadata<T> {
+  object: O
+}
+
+export default function inject<T extends (...args: any[]) => any, P extends Parameters<T>, R extends ReturnType<T>, K extends keyof R, N extends Parameters<R[K]>>(target: T, cb: (meta: Metadata<T>, ...args: P) => R | undefined | void): T
+export default function inject<T extends (...args: any[]) => any, P extends Parameters<T>, R extends ReturnType<T>, K extends keyof R, N extends Parameters<R[K]>>(prototype: R, method: K, cb: (meta: PrototypeMetadata<R[K], R>, ...args: N) => ReturnType<R[K]> | undefined | void): T
+export default function inject<T extends (...args: any[]) => any, P extends Parameters<T>, R extends ReturnType<T>, K extends keyof R, N extends Parameters<R[K]>>(target: T | R, cbm: K | ((meta: Metadata<T>, ...args: P) => R | undefined | void), cb?: (meta: PrototypeMetadata<R[K], R>, ...args: N) => ReturnType<R[K]> | undefined | void): T {
+  let noreturn = false
+  if (typeof cbm === 'symbol' || typeof cbm === 'string' || typeof cbm === 'number') {
+    const original = (<R>target)[cbm]
+    ;(<R>target)[cbm] = function (...args: N) {
+      const modifiedReturn = cb?.({
+        stacktrace: parse(new Error()).slice(1),
+        target: original.bind(this),
+        noreturn: () => noreturn = true,
+        object: this
+      }, ...args)
+      return noreturn ? undefined : (modifiedReturn ?? (original.bind(this))(...args))
     }
-    const modifiedReturn = cb(metadata, ...args)
-    return !noreturn && (modifiedReturn ?? target(...args))
-  }) as T
+    return (<R>target)[cbm]
+  } else {
+    return ((...args: P) => {
+      const modifiedReturn = cbm({
+        stacktrace: parse(new Error()).slice(1),
+        target,
+        noreturn: () => noreturn = true
+      }, ...args)
+      return noreturn ? undefined : (modifiedReturn ?? (<T>target)(...args))
+    }) as T
+  }
+}
